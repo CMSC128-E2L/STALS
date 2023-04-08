f687756d90a70c887f7b48e9f42580fca7813919
Van Paul Dayag <vcdayag@up.edu.ph>
2023-04-08 20:51:28 +0800

    feat: implement file upload api

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
 import { userRouter } from "~/server/api/routers/user";
 import { accomodationRouter } from "~/server/api/routers/accomodation";
 import { roomRouter } from "~/server/api/routers/room";
+import { fileRouter } from "~/server/api/routers/file";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   user: userRouter,
   accomodation: accomodationRouter,
   room: roomRouter,
+  file: fileRouter,
 });
 
 // export type definition of API
