d45dedf053285de44298ad0e35736d897ad14e6e
Van Paul Dayag <vcdayag@up.edu.ph>
2023-04-23 17:30:00 +0800

    back: remove routes in review router

--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -37,19 +37,4 @@ export const reviewRouter = createTRPCRouter({
         });
     }),
 
-    hello: publicProcedure
-        .input(z.object({ text: z.string() }))
-        .query(({ input }) => {
-            return {
-                greeting: `Hello ${input.text}`,
-            };
-        }),
-
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.example.findMany();
-    }),
-
-    getSecretMessage: protectedProcedure.query(() => {
-        return "you can now see this secret message!";
-    }),
 });
