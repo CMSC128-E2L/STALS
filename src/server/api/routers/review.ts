b4dd99e6bbe3207c5ce95c409eb08fdb86c169a9
vcdayag <100204084+vcdayag@users.noreply.github.com>
2023-04-23 17:30:57 +0800

    back: cleanup review router (#60)
    * back: simplify addReview
    
    * style: format review.ts
    
    * back: remove routes in review router

--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
 } from "~/server/api/trpc";
 
 export const reviewRouter = createTRPCRouter({
@@ -11,56 +11,30 @@ export const reviewRouter = createTRPCRouter({
     addReview: protectedProcedure
         .input(
             z.object({
-            accommodationId: z.string(),
-            review: z.string(),
-            rating: z.number(),
-            username: z.string().optional(),
+                accommodationId: z.string(),
+                review: z.string(),
+                rating: z.number(),
             })
         )
         .mutation(({ ctx, input }) => {
             const userId = ctx?.session?.user?.id;
             const { accommodationId, review, rating } = input;
-            if (userId) {
-                return ctx.prisma.review.create({
+
+            return ctx.prisma.review.create({
                 data: {
-                    accommodation: { connect: { id: accommodationId } },
-                    review,
-                    rating,
                     user: { connect: { id: userId } },
-                },
-                });
-            } else {
-                return ctx.prisma.review.create({
-                data: {
-                    userName: input.username,
                     accommodation: { connect: { id: accommodationId } },
                     review,
                     rating,
                 },
-                });
-            }
+            });
         }),
 
     getMany: publicProcedure.input(z.string()).query(({ ctx, input }) => {
         const accommodationId = input;
-            return ctx.prisma.review.findMany({
-                where: { accommodationId },
-            });
-        }),
-
-    hello: publicProcedure
-        .input(z.object({ text: z.string() }))
-        .query(({ input }) => {
-        return {
-            greeting: `Hello ${input.text}`,
-        };
-        }),
-
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.example.findMany();
+        return ctx.prisma.review.findMany({
+            where: { accommodationId },
+        });
     }),
 
-    getSecretMessage: protectedProcedure.query(() => {
-        return "you can now see this secret message!";
-    }),
 });
