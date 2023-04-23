11f155c083e3a41de96ef605d4c78c83fdb65ac8
Van Paul Dayag <vcdayag@up.edu.ph>
2023-04-23 17:35:03 +0800

    style(back): format report router

--- src/server/api/routers/report.ts
+++ src/server/api/routers/report.ts
@@ -9,50 +9,50 @@ import {
 export const reportRouter = createTRPCRouter({
   report: protectedProcedure
     .input(
-        z.object({ 
-            type_reported: z.string(),
-            reported_id: z.string(),
-            report: z.string() 
-        })
+      z.object({
+        type_reported: z.string(),
+        reported_id: z.string(),
+        report: z.string()
+      })
     )
     .mutation(({ ctx, input }) => {
-        const user_id = ctx?.session?.user?.id;
-        const { type_reported, reported_id, report } = input;
-        return ctx.prisma.report.create({
-            data: {
-                user: { connect: { id: user_id } },
-                type_reported,
-                reported_id,
-                report,
-            }
-        });
+      const user_id = ctx?.session?.user?.id;
+      const { type_reported, reported_id, report } = input;
+      return ctx.prisma.report.create({
+        data: {
+          user: { connect: { id: user_id } },
+          type_reported,
+          reported_id,
+          report,
+        }
+      });
     }),
 
-/* OH NAURR! not sure daw but don't delete! >__<
+  /* OH NAURR! not sure daw but don't delete! >__<
+    
+      reportUser: publicProcedure
+          .input(z.object({ text: z.string() }))
+          .query(({ input }) => {}),
   
-    reportUser: publicProcedure
-        .input(z.object({ text: z.string() }))
-        .query(({ input }) => {}),
-
-    reportReview: publicProcedure
-        .input(z.object({ text: z.string() }))
-        .query(({ input }) => {}),
-
-*/
+      reportReview: publicProcedure
+          .input(z.object({ text: z.string() }))
+          .query(({ input }) => {}),
+  
+  */
 
   getManyReports: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
 
-/* NAURR hindi rin daw sure!! UGH >__<
-
-  getManyUsers: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-  getManyReviews: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
-*/
+  /* NAURR hindi rin daw sure!! UGH >__<
+  
+    getManyUsers: publicProcedure.query(({ ctx }) => {
+      return ctx.prisma.example.findMany();
+    }),
+    getManyReviews: publicProcedure.query(({ ctx }) => {
+      return ctx.prisma.example.findMany();
+    }),
+  
+  */
 
 });
