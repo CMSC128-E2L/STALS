52f515d012e0658bfae3a7eee04f1a50d2af577f
Van Paul Dayag <vcdayag@up.edu.ph>
2023-04-25 21:55:09 +0800

    fix(front): use nextjs Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -16,7 +17,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-start justify-start bg-gradient-to-b from-[#174A79] to-[#8FB0DB]">
         <div className="container flex flex-col items-start justify-start gap-8 px-4 py-16">
-          <img
+          <Image
             src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
             className="flex h-40 w-40 flex-col items-start rounded-full object-scale-down object-left"
             alt="STALS Logo"
