dba5b235af0d68fa0542842b7c5165cca367a90b
jararellano <jrarellano1@up.edu.ph>
2023-04-27 00:22:09 +0800

    better form scalability

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,7 @@ export default function Login() {
     <div>
       {/* Header design */}
       <header>
-        <div className="h-[20rem] bg-[url('https://images.pexels.com/photos/1446378/pexels-photo-1446378.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-no-repeat">
+        <div className="h-halfscreen bg-[url('https://images.pexels.com/photos/1446378/pexels-photo-1446378.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-no-repeat">
           {/* <h1 className="text-center text-white pt-12 text-4xl"><b>Welcome back!</b></h1> */}
           <div className="h-[100%] bg-gradient-to-b from-transparent to-black opacity-80"></div>
         </div>
@@ -15,8 +15,8 @@ export default function Login() {
         </p>
       </header>
       {/* Container for log-in credentials */}
-      <div className="absolute inset-x-0 top-0 mt-32 flex content-center justify-center">
-        <div className="w-1/3 rounded-xl bg-white p-20 shadow shadow-p-black/50">
+      <div className="absolute inset-x-0 top-32 flex justify-center">
+        <div className="w-auto rounded-xl bg-white p-10 shadow shadow-p-black/50">
           <form>
             <div className="flex flex-col space-y-4">
               <input
@@ -39,20 +39,8 @@ export default function Login() {
             <button className="group relative flex w-full justify-center rounded-lg bg-p-dblue px-4 py-2 font-bold text-white">
               Log In
             </button>
-            <br />
-            <p className="text-center text-sm text-gray-400">
-              If you are having issues logging in, please contact an{" "}
-              <a className="text-cyan-500 underline" href="">
-                administrator
-              </a>
-              .
-            </p>
-            <br />
             {/* Log-in, help, sign-up buttons */}
             <div>
-              <button className="group relative flex w-full justify-center rounded-lg bg-p-dblue px-4 py-2 font-bold text-white">
-                Log In
-              </button>
               <br />
               <p className="text-center text-sm text-gray-400">
                 If you are having issues logging in, please contact an{" "}
