2150433360bc15ec89f08fe95e26defb9a25de79
Jamie Mari Ciron <jociron@up.edu.ph>
2023-04-29 21:46:28 +0800

    Revert "chore: adjusted elements in navbar"
    This reverts commit 07bacbedd0007a9c919aeb644590a03fdac72ba5.

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -3,42 +3,63 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div>
-      {/* Navigation bar */}
-      <nav className="bg-p-dblue">
-        <div className="mx-auto flex flex-wrap items-center justify-between p-4">
-          {/* Left side */}
-          <div className="w-full items-center md:block md:w-auto">
-            <a href="" className="flex items-center">
-              <img
-                src="https://www.pngfind.com/pngs/m/439-4392840_facebook-link-icon-image-dynamic-spectrum-alliance-pink.png"
-                className="mr-3 h-10 rounded-3xl"
-                alt="STALS Logo"
-              />
-              <h1 className="self-center text-4xl font-bold text-white">
-                STALS
-              </h1>
-            </a>
-          </div>
-          {/* Right side */}
-          <div className="w-full md:block md:w-auto">
-            <ul className="mt-4 flex flex-col items-center p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 ">
-              <li>
-                <input
-                  className="rounded-2xl px-3 py-1"
-                  placeholder="Search"
-                ></input>
-              </li>
-              <li>
-                <img
-                  src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-                  className="h-10 rounded-3xl"
-                  alt="Profile"
-                />
-              </li>
-            </ul>
-          </div>
+      <div className="flex justify-between bg-p-dblue px-8 py-8">
+        {" "}
+        {/* navigation */}
+        <div className="flex">
+          {" "}
+          {/* left side */}
+          <Image
+            className="h-[50px] "
+            src="https://www.pngfind.com/pngs/m/439-4392840_facebook-link-icon-image-dynamic-spectrum-alliance-pink.png"
+            alt="STALS Logo"
+          />
+          <h1 className="text-[30px]">STALS</h1>
         </div>
-      </nav>
+        <div className="flex">
+          {" "}
+          {/* right side */}
+          <input className=" rounded-lg " placeholder="Search"></input>
+          <h1 className="text-[30px]">STALS</h1>
+        </div>
+      </div>
     </div>
   );
 }
+
+//  {/* Container for log-in credentials */}
+//  <div className="absolute inset-0 mt-32 flex content-center justify-center">
+//  <div className="h-[28rem] w-1/3 bg-white rounded-xl p-20 shadow shadow-p-black/50">
+//    <form>
+//      <div className="flex flex-col space-y-4">
+//        <input
+//          name="Email"
+//          type="email"
+//          placeholder="Email"
+//          className="rounded-xl px-3 py-3 shadow shadow-p-black/50"
+//        />
+//        <input
+//          name="Password"
+//          type="password"
+//          placeholder="Password"
+//          className="rounded-xl px-3 py-3 shadow shadow-p-black/50"
+//        />
+//      </div>
+//    </form>
+//    <br />
+//    {/* Log-in, help, sign-up buttons */}
+//    <div>
+//      <button className="group relative flex w-full justify-center rounded-lg bg-p-dblue px-4 py-2 text-white">
+//        <b>Log In</b>
+//      </button>
+//      <br />
+//      <p className="text-center text-sm text-gray-400">
+//        If you are having issues logging in, please contact an <a className="underline text-cyan-500" href="">administrator</a>.
+//      </p>
+//      <br />
+//      <p className="text-center text-sm text-gray-400">
+//        Don't have an account yet? <a className="underline text-cyan-500" href="">Sign Up</a>.
+//      </p>
+//    </div>
+//  </div>
+// </div>
