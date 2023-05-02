3d63655ad53863b02a2985ae9c58bbcc3a9ddc43
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-02 19:09:08 +0800

    chore(front): cleanup accommodation testing page

--- src/pages/test/accommodation.tsx
+++ src/pages/test/accommodation.tsx
@@ -1,10 +1,4 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-
 import { api } from "~/utils/api";
-import { stringify } from "superjson";
 
 export default function Backend() {
   const createAccommodation = api.accommodation.add.useMutation();
