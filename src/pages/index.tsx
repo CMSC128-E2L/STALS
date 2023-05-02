2d0dc17212a3beb8e38cc735cef7575894ba6cc6
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-02 19:06:08 +0800

    chore(front): cleanup index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import { type NextPage } from "next";
-import Image from "next/image";
 import Head from "next/head";
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "~/utils/api";
-
 const Home: NextPage = () => {
-  // const hello = api.user.hello.useQuery({ text: "from STALS Developers" });
   return (
     <>
       <Head>
@@ -71,16 +66,10 @@ export default Home;
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  // const { data: secretMessage } = api.user.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined },
-  // );
-
   return (
     <div className="flex flex-col items-start justify-start gap-4">
       <p className="text-start text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {/* {secretMessage && <span> - {secretMessage}</span>} */}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
