f20d3cb35779c24813df13307a667f66f8dafb9a
kbdaine <kbpunzalan@up.edu.ph>
2023-05-03 18:41:47 +0800

    fix (frontend): homepage colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
         "p-lblue": "#BBCDE5", // Light blue
         "p-red": "#D22B2B",
         "p-yellow": "#F5BB00",
+        "p-gray": "#D9D9D9",
       },
       fontSize: {
         xs: ["12px", "12px"],
