b448a7dce7bf00cda17ea776304e60bb45d71922
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-05 13:40:02 +0800

    fix: temporarily disable R2 dependency

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,9 +4,9 @@ import { env } from "~/env.mjs";
 // Set the AWS Region.
 export const s3Client = new S3Client({
   region: "auto",
-  endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: `https://${env.R2_ACCOUNT_ID ?? ""}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: env.R2_ACCESS_KEY,
-    secretAccessKey: env.R2_SECRET_ACCESS_KEY,
+    accessKeyId: env.R2_ACCESS_KEY ?? "",
+    secretAccessKey: env.R2_SECRET_ACCESS_KEY ?? "",
   },
 });
