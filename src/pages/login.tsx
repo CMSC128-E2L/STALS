29bb79fb916b521beccb9b3fb21ead1fa420ae9d
Jamie Mari Ciron <71953730+jmociron@users.noreply.github.com>
2023-05-06 21:46:32 +0800

    chore: edited links in login page (#159)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,6 +7,7 @@ import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "~/server/auth";
 import logo from "public/images/logo.png";
+import Link from "next/link";
 
 export default function SignIn({
   providers,
@@ -15,12 +16,11 @@ export default function SignIn({
     <div>
       <header>
         <div className="absolute inset-0 h-[50%] bg-[url('https://www.camellahomes.net/wp-content/uploads/2022/01/camella-homes-header.jpg')] bg-cover bg-no-repeat">
-          {/* <h1 className="text-center text-white pt-12 text-4xl"><b>Welcome back!</b></h1> */}
           <div className="h-[100%] bg-gradient-to-b from-transparent to-black opacity-50"></div>
         </div>
         <h1 className="absolute inset-10 text-center text-5xl font-bold text-white drop-shadow-md">
           <div className="flex h-20 w-auto justify-center drop-shadow-md">
-            <img src={logo.src} alt="logo" />
+            <img src={logo.src} alt="STALS Logo" />
           </div>
           Welcome back! <br />
           <p className="text-center text-lg font-bold text-white drop-shadow-md">
@@ -33,30 +33,31 @@ export default function SignIn({
           <div className="flex flex-col space-y-4">
             {Object.values(providers).map((provider) => (
               <div key={provider.name}>
-                {/* <button className= "group relative flex w-full justify-center rounded-lg bg-white px-4 py-2 font-bold text-p-black shadow shadow-gray-400/100" */}
                 <button
                   className={`${provider.id}`}
                   onClick={() => void signIn(provider.id)}
                 >
                   <img
-                    src={`assets/${provider.id}.png`}
+                    src={`/assets/${provider.id}.png`}
                     className="mr-3 mt-0.5 h-5 align-baseline"
-                    alt={`${provider.id} logo`}
+                    alt={`${provider.name} Logo`}
                   ></img>
                   Sign in with {provider.name}
                 </button>
               </div>
             ))}
-            <button className="group relative flex w-full justify-center rounded-md bg-slate-500 px-4 py-2 font-bold text-white shadow shadow-gray-400/100">
-              Continue as guest
-            </button>
+            <Link href="/homepage">
+              <button className="group relative flex w-full justify-center rounded-full bg-slate-500 px-4 py-2 font-bold text-white shadow shadow-gray-400/100">
+                Continue as guest
+              </button>
+            </Link>
           </div>
           <div>
             <p className="mt-5 text-center text-sm text-gray-400">
               Don&apos;t have an account yet?{" "}
-              <a className="text-cyan-500 underline" href="signup">
+              <Link className="text-cyan-500 underline" href="/signup">
                 Sign up
-              </a>
+              </Link>
             </p>
           </div>
         </div>
