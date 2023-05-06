65081726c241a5b20ead341a2458ce9fcae41113
Sadie <54937019+SadieBeltran@users.noreply.github.com>
2023-05-06 12:06:58 +0800

    feat(font): Added Accommodation Page (#148)
    * feat: Added Accommodation Page
    
    * feat: fleshed out Accommodation page more
    
    * refactor: fiddled with button toggles

---
+++ src/pages/accommodationPage.tsx
@@ -0,0 +1,243 @@
+export default function Accommodation() {
+  return (
+    <div className="temp">
+      {/* HEADER */}
+      <header className="navbar-header">
+        {/* Left side */}
+        <div className="items-center align-middle">
+          <a href="" className="flex items-center">
+            <img
+              src="https://www.pngfind.com/pngs/m/439-4392840_facebook-link-icon-image-dynamic-spectrum-alliance-pink.png"
+              className="mr-3 h-12 rounded-3xl"
+              alt="STALS Logo"
+            />
+            <h1 className="self-center text-4xl font-bold text-white">STALS</h1>
+          </a>
+        </div>
+
+        {/* Right side */}
+        <div className="w-full md:block md:w-auto">
+          <ul className="mt-4 flex flex-col items-center p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 ">
+            <li>
+              <input
+                className="rounded-2xl px-3 py-1"
+                placeholder="Search"
+              ></input>
+            </li>
+            <li>{/* image placeholder */}</li>
+          </ul>
+        </div>
+      </header>
+      {/* BODY */}
+      <div>
+        {/* LANDLORD PROFILE */}
+        <div className="position-left ml-10 w-1/4 bg-yellow-100 p-4 py-2">
+          <div className="grid grid-cols-2 rounded-md bg-white px-10 shadow-md">
+            {/* Insert Landlord Image here */}
+            <img
+              src="public/placeholder_1.png"
+              className="mr-3 h-12 rounded-full"
+            ></img>
+            <div>
+              <h1 className="name"> Alfred Santos</h1>
+              <label>Date | time</label>
+            </div>
+          </div>
+        </div>
+        {/* cONTAINS THE ACCOMMODATION INFO */}
+        <div className="flex min-h-screen justify-center">
+          {/* Box that contains the accommodation thingy */}
+          <div className="margin-40 flex w-11/12 gap-2 bg-p-lblue p-4 py-4 shadow-md">
+            {/* GALLERY */}
+            <div className="w-1/3 flex-none bg-red-100">a</div>
+
+            {/* DESCRIPTION */}
+            <div className="flex w-3/4 flex-initial flex-col bg-green-100 p-4">
+              {/* ACCOMMODATION NAME + edit + delete thngy idk*/}
+              <div className="flex flex-row items-stretch bg-purple-100">
+                {/* Left column (accommodation name) */}
+                <div className="flex w-3/4 items-center px-3">
+                  <h1 className="form-h1">Accommodation Name</h1>
+                </div>
+
+                {/* Right column: the editing thingy ig */}
+                <div className="basis-1/4">
+                  {/* So if a registered user is viewing it (remove hidden to show teehee)*/}
+                  <div className="float-right inline-flex cursor-pointer">
+                    <form>
+                      <input
+                        type="checkbox"
+                        value="favorite"
+                        className="peer sr-only"
+                      ></input>
+                      {/* hover: active-within: static rounded-full bg-transparent fill-p-red p-2 opacity-75 transition hover:bg-p-red */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="h-8 w-8 transition hover:h-9 hover:w-9 peer-checked:fill-p-red peer-checked:stroke-p-red"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </form>
+                  </div>
+
+                  {/* If a landlord is viewing the page */}
+                  <div className="float-right flex hidden gap-1">
+                    {/* Edit button */}
+                    <button type="button" className="accomP-button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-8 w-8 fill-white stroke-p-dblue"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                    </button>
+                    {/* Archive button */}
+                    <button type="button" className="accomP-button ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-8 w-8 fill-white stroke-p-dblue"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                        />
+                      </svg>
+                    </button>
+
+                    {/* Delete button */}
+                    <button type="button" className="accomP-button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-8 w-8 fill-white stroke-p-dblue"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              {/* ACCOMMODATION DESCRIPTION */}
+              <div className="bg-purple-300 px-4 text-xl italic">
+                Accommodation Type
+              </div>
+
+              {/* STATS */}
+              <div className="flex flex-row gap-2 bg-red-200 px-3 text-sm">
+                <div className="flex flex-row items-center gap-x-1 bg-blue-200 p-1">
+                  <div className="rounded-full bg-p-dblue p-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-5 w-5 fill-white stroke-p-dblue"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                  </div>
+                  Phone Number
+                </div>
+                <div className="flex flex-row items-center gap-x-1 bg-cyan-200 p-1">
+                  <div className="rounded-full bg-p-dblue p-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-5 w-5 fill-white stroke-p-dblue"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                      />
+                    </svg>
+                  </div>
+                  Email
+                </div>
+                <div className="flex flex-row items-center gap-x-1 bg-yellow-200 p-1">
+                  <div className="rounded-full bg-p-dblue p-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-5 w-5 stroke-white"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                      />
+                    </svg>
+                  </div>
+                  Address
+                </div>
+              </div>
+
+              {/* DESCRIPTION */}
+              <div>
+                <div className="peer:checked group flex h-1/4 flex-col overflow-hidden bg-orange-300">
+                  <p className="max-h-3/4 line-clamp-3 max-w-prose break-words break-all px-8 py-4">
+                    Before him, the ten greatest sects in the cultivation world
+                    had divided territories, fought and hoarded against each
+                    other over their domains. With the sects clashing against
+                    one another, there was no one who could rule the world and
+                    call all the shots. And besides, every sect leader were
+                    outstandingly learned, so even if they wanted to grant
+                    themselves a title for fun, they would be wary of what the
+                    historians would pen, afraid they would receive disgrace in
+                    the books of history.
+                  </p>
+                </div>
+                <input type="checkbox peer"></input>Click
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
