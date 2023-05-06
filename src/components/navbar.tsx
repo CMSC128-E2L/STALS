3912ef75eb1d4035990ba42b1014327cc7edd7aa
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-06 12:27:01 +0800

    feat(front): use navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
@@ -7,14 +8,14 @@ export default function NavBar() {
       <nav className="sticky top-0 mx-auto mb-5 flex flex-wrap items-center justify-between bg-p-dblue p-4">
         {/* Left side */}
         <div className="items-center align-middle">
-          <a href="" className="flex items-center">
+          <Link href="/homepage" className="flex items-center">
             <img
               src="https://www.pngfind.com/pngs/m/439-4392840_facebook-link-icon-image-dynamic-spectrum-alliance-pink.png"
               className="mr-3 h-12 rounded-3xl"
               alt="STALS Logo"
             />
             <h1 className="self-center text-4xl font-bold text-white">STALS</h1>
-          </a>
+          </Link>
         </div>
 
         {/* Right side */}
--- src/pages/addAccommodation.tsx
+++ src/pages/addAccommodation.tsx
@@ -1,33 +1,10 @@
+import NavBar from "~/components/navbar";
+
 export default function AddAccommodation() {
   return (
     <div className="thing">
       {/* Header */}
-      <header className="navbar-header">
-        {/* Left side */}
-        <div className="items-center align-middle">
-          <a href="" className="flex items-center">
-            <img
-              src="https://www.pngfind.com/pngs/m/439-4392840_facebook-link-icon-image-dynamic-spectrum-alliance-pink.png"
-              className="mr-3 h-12 rounded-3xl"
-              alt="STALS Logo"
-            />
-            <h1 className="self-center text-4xl font-bold text-white">STALS</h1>
-          </a>
-        </div>
-
-        {/* Right side */}
-        <div className="w-full md:block md:w-auto">
-          <ul className="mt-4 flex flex-col items-center p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 ">
-            <li>
-              <input
-                className="rounded-2xl px-3 py-1"
-                placeholder="Search"
-              ></input>
-            </li>
-            <li>{/* image placeholder */}</li>
-          </ul>
-        </div>
-      </header>
+      <NavBar />
 
       {/* Body */}
       <div className="basis-1/8">
