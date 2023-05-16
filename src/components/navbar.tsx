7ed4f5e014aa55a1419cdf6fcc59a4a427281762
Jamie Mari Ciron <jociron@up.edu.ph>
2023-05-16 11:37:13 +0800

    feat: moved navbar back to homepage

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,12 +30,8 @@ export default function NavBar() {
         {/* Right side */}
         <div className="w-full md:block md:w-auto">
           <ul className="mr-2 mt-4 flex flex-col items-center p-4 font-medium md:mt-0 md:flex-row md:space-x-5 md:border-0 md:p-0">
-            <li>
-              <input
-                className="rounded-full px-3 py-1"
-                placeholder="Search"
-              ></input>
-            </li>
+            {/* <li>
+            </li> */}
             <li>
               <ProfileButton />
             </li>
