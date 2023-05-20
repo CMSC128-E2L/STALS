6b3bd234b6a18a7473ae2d6344265e292876790b
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-20 11:14:11 +0800

    fix: workaround add validate component in the uppermost file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Validate from "./validate";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Validate />
       <Component {...pageProps} />
     </SessionProvider>
   );
