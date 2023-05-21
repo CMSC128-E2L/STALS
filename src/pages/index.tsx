828f995eea2edcd16e8bacf11e3f6960854da5be
vincentkr16 <69850353+vincentkr16@users.noreply.github.com>
2023-05-21 22:29:30 +0800

    feat(front): added new background for login, signup, and landing page (#263)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import graphics from "public/images/pic.png";
-import bg from "public/images/bg.png";
+import bg from "public/images/landing-bg-02.png";
 import logo from "public/images/logo.png";
 
 const Home: NextPage = () => {
@@ -17,22 +17,19 @@ const Home: NextPage = () => {
       </Head>
 
       {/* layout whole page */}
-      <main className="min-h-screen bg-gradient-to-b from-[#174A79] to-[#BBCDE5]">
+      <main className="">
         {/* landing page whole screen content */}
         <div className="flex flex-col">
           {/* graphics */}
-          <div className="absolute bottom-1 right-1/2 h-[100%] w-screen translate-x-1/2 opacity-70">
-            <Image className="object-fill" src={bg} alt="background" fill />
-          </div>
-          {/* sample image only */}
-          <div className="absolute bottom-1 right-1/2 h-[35%] w-screen translate-x-1/2">
-            <Image
-              className="object-contain"
-              src={graphics}
-              alt="graphics"
-              fill
-            />
-          </div>
+          {/* <div className="absolute bottom-1 right-1/2 h-[100%] w-screen translate-x-1/2 opacity-70">
+            <Image className="" src={bg} alt="background" fill />
+          </div> */}
+
+          <img
+            className="absolute w-screen bg-cover bg-fixed bg-center"
+            src={bg.src}
+            alt="background"
+          />
 
           {/* 1 header nav bar [logo name home about contact]*/}
           <header className="sticky top-0 justify-between">
@@ -78,11 +75,11 @@ const Home: NextPage = () => {
           {/* lowest prio: nagsslide yung description [description -> full name] */}
           {/* 2 main content after header navbar [title subtitle get started button] */}
           <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col justify-center text-center">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            <h1 className="pb-4 text-5xl font-extrabold tracking-tight text-white drop-shadow-md sm:text-[5rem]">
               Comfort At Its Best!
             </h1>
 
-            <p className="p-5 font-sans text-lg leading-normal tracking-wider text-white">
+            <p className="p-2 font-sans text-lg leading-normal tracking-wider text-white drop-shadow-md">
               You can search, get information, review, and <br />
               rate apartments, bed spaces, hotels, dormitories,
               <br /> and transient spaces in Los Baños.
