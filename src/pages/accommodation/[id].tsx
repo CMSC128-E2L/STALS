5866c4d7f955a45a4727da8047a96b3db8649d87
Seiichiro Mine <etbeltran1@up.edu.ph>
2023-05-22 01:29:06 +0800

    feat (front): Added Add room button

--- src/pages/accommodation/[id].tsx
+++ src/pages/accommodation/[id].tsx
@@ -350,7 +350,24 @@ export default function Accommodation() {
                   <RoomButton />
                   <RoomButton />
                   <RoomButton />
-                  <RoomButton />
+                  {/* TODO: ADD ROOM BUTTON SHOULD ONLY APPEAR IF LANDLORD IS LOOKING AT PAGE */}
+                  <button className="flex flex-col items-center rounded-lg border-2 border-dashed border-p-black/50 px-8">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                    <label className="text-xs">Add Room</label>
+                  </button>
                 </div>
               </div>
 
