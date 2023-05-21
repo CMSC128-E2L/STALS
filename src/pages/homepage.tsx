3db4f9a7e6c190d9c2e9af883e4798301516e622
lawmarquez <lumarquez@up.edu.ph>
2023-05-22 01:49:47 +0800

    feat(front): Usertype in signup

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -174,7 +174,7 @@ export default function HomePage() {
                   onChange={handlePriceRangeChange}
                   className="ml-3 h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
                 />
-                <label htmlFor="below-1000" className="filter-text">
+                <label htmlFor="all" className="filter-text">
                   All
                 </label>
               </div>
