d6984fdc22e07196b3948aeea7b11e0e39756c52
lawmarquez <lumarquez@up.edu.ph>
2023-05-22 03:56:53 +0800

    fix(front): correct typo of Suffix

--- src/utils/apitypes.ts
+++ src/utils/apitypes.ts
@@ -36,7 +36,7 @@ export const userEditSchema = z.object({
   first_name: z.string().min(1).optional(),
   middle_name: z.string().min(1).optional(),
   last_name: z.string().min(1).optional(),
-  suffix: z.string().optional(),
+  Suffix: z.string().optional(),
   username: z.string().min(8).optional(),
   contact_number: z.string().min(10).optional(),
   type: z.nativeEnum(UserType),
