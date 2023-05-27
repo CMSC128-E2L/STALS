c98f84db8f27b33d0bf472959ee52d499bd6a2c1
acgarcia11 <ashlee@LAPTOP-IACPJK6S.localdomain>
2023-05-27 16:13:11 +0800

    feat(front): edit prompts in edit profile page

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -5,9 +5,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { userEditSchema } from "~/utils/apitypes";
 import bgpic from "public/images/bg-05.png";
-import Link from "next/link";
 import NavBar from "~/components/navbar";
 import { useState } from "react";
+import ConfirmationPrompt from "~/components/prompt";
 
 const EditProfile: NextPage = () => {
   const userSession = useSession();
@@ -34,7 +34,17 @@ const EditProfile: NextPage = () => {
     },
   });
 
-  const [showPrompt, setShowPrompt] = useState(false);
+  const [showDeacPrompt, setShowDeacPrompt] = useState(false);
+  const [showDiscardPrompt, setShowDiscardPrompt] = useState(false);
+  const [isChanged, seIsChanged] = useState(false);
+
+  const handleCancel = () => {
+    if (isChanged) {
+      setShowDiscardPrompt(true);
+    } else {
+      window.location.replace("/profile");
+    }
+  };
 
   return (
     <div>
@@ -61,6 +71,7 @@ const EditProfile: NextPage = () => {
                 console.log(d);
                 editUser.mutate(d as RouterInputs["user"]["edit"]);
               })}
+              onChange={() => seIsChanged(true)}
             >
               <div className="flex flex-col space-y-2.5">
                 <div className="w-auto flex-row space-x-[2%]">
@@ -129,11 +140,12 @@ const EditProfile: NextPage = () => {
                   </button>
                 </div>
                 <div>
-                  <Link href="/profile">
-                    <button className="group relative flex w-full justify-center rounded-full bg-slate-500 px-4 py-2 font-bold text-white shadow shadow-gray-400/100">
-                      Cancel
-                    </button>
-                  </Link>
+                  <button
+                    className="group relative flex w-full justify-center rounded-full bg-slate-500 px-4 py-2 font-bold text-white shadow shadow-gray-400/100"
+                    onClick={handleCancel}
+                  >
+                    Cancel
+                  </button>
                 </div>
               </div>
             </form>
@@ -142,82 +154,32 @@ const EditProfile: NextPage = () => {
               data-modal-target="prompt"
               className="mt-2 flex w-full justify-center rounded-3xl border-2 border-red-600 p-2 text-red-600 shadow-lg"
             >
-              <button className="w-full " onClick={() => setShowPrompt(true)}>
+              <button
+                className="w-full "
+                onClick={() => setShowDeacPrompt(true)}
+              >
                 Deactivate Account
               </button>
             </div>
           </div>
         </div>
-
-        {showPrompt && (
-          <center>
-            <div
-              id="prompt"
-              className="absolute z-50 h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden p-4 backdrop-blur-sm md:inset-0"
-            >
-              <div className="relative max-h-full w-full max-w-md">
-                <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
-                  <button
-                    type="button"
-                    className="absolute right-2.5 top-3 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white"
-                    data-modal-hide="popup-modal"
-                    onClick={() => setShowPrompt(false)}
-                  >
-                    <svg
-                      aria-hidden="true"
-                      className="h-5 w-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    <span className="sr-only">Close modal</span>
-                  </button>
-                  <div className="p-6 text-center">
-                    <svg
-                      aria-hidden="true"
-                      className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                      ></path>
-                    </svg>
-                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                      Are you sure you want to delete this account?
-                    </h3>
-                    <button
-                      onClick={() => void deleteProfile.mutate()}
-                      data-modal-hide="popup-modal"
-                      type="button"
-                      className="mr-2 inline-flex items-center rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800"
-                    >
-                      Confirm
-                    </button>
-                    <button
-                      onClick={() => setShowPrompt(false)}
-                      data-modal-hide="popup-modal"
-                      type="button"
-                      className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-600"
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </center>
+        {showDiscardPrompt && (
+          <ConfirmationPrompt
+            onConfirm={() => {
+              window.location.replace("/profile");
+            }}
+            onCancel={() => setShowDiscardPrompt(false)}
+            message="Discard changes?"
+          />
+        )}
+        {showDeacPrompt && (
+          <ConfirmationPrompt
+            onConfirm={() => {
+              void deleteProfile.mutate();
+            }}
+            onCancel={() => setShowDeacPrompt(false)}
+            message="Are you sure you want to delete this account?"
+          />
         )}
       </div>
     </div>
