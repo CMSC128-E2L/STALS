777ee4c34f95308a15c2d5dd310350fae243a3ad
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-28 02:45:40 +0800

    feat: implement file uploading

---
+++ src/pages/upload.tsx
@@ -0,0 +1,31 @@
+export default function Upload() {
+  return (
+    <>
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={(e) => void uploadPhoto(e)}
+      />
+    </>
+  );
+}
+
+const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
+  const file = e.target.files?.[0]!;
+  const filename = file.name;
+  const fileType = file.type;
+
+  const res = await fetch(`/api/upload?file=${filename}&fileType=${fileType}`);
+  const { url } = (await res.json()) as { url: string };
+
+  const uploading = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": fileType,
+    },
+    body: file,
+  });
+
+  console.log(uploading.status);
+};
