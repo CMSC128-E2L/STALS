293fed72e1864af5bbbf1c912a0528be60ee2965
Kath Cabrera <dccabrera2@up.edu.ph>
2023-05-28 19:20:45 +0800

    chore:Added icon to Report button

--- src/pages/accommodation/[id]/index.tsx
+++ src/pages/accommodation/[id]/index.tsx
@@ -489,22 +489,23 @@ export default function Accommodation() {
         {/*Report button*/}
         <div>
           <button
-            className="xs m-10 rounded-full bg-sky-200 p-3"
+            className="xxs m-5 space-y-10 rounded-full p-3"
             // onClick={() => {;}}
           >
             Report a Problem
             <svg
+              xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="h-5 w-5 fill-white stroke-p-dblue"
+              className="h-6 w-6"
             >
-              {/* <path
+              <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-              /> */}
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+              />
             </svg>
           </button>
         </div>
