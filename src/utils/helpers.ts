920160abbc7ad752f6259991087399996e75a575
vcdayag <100204084+vcdayag@users.noreply.github.com>
2023-05-28 12:50:47 +0800

    feat: add return types for helper functions (#410)

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,14 +1,17 @@
 import { type Prisma } from "@prisma/client";
 import { type NextRouter } from "next/router";
 
-export function dynamicRouteID(router: NextRouter) {
+export function dynamicRouteID(router: NextRouter): {
+  shouldReturn: boolean;
+  id: string;
+} {
   if (!router.query) return { shouldReturn: true, id: "" };
   const { id } = router.query as { id: string };
   if (typeof id == "undefined") return { shouldReturn: true, id: "" };
   return { shouldReturn: false, id };
 }
 
-export function titleCase(word: string) {
+export function titleCase(word: string): string {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
@@ -20,6 +23,6 @@ export function stalsDBstringArray(json?: Prisma.JsonValue): string[] {
   return [];
 }
 
-export function notAuthenticated(status: string) {
+export function notAuthenticated(status: string): boolean {
   return status !== "authenticated";
 }
