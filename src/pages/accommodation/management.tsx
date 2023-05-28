a8b2507d54219e5e8b0711156dc0bce99dd4055a
jmrqno <jrraqueno@up.edu.ph>
2023-05-28 16:19:06 +0800

    WIP (back): integrates component to management.tsx

--- src/pages/accommodation/management.tsx
+++ src/pages/accommodation/management.tsx
@@ -44,77 +44,15 @@ export default function Delete_Archive_Accomm() {
             <div className="mx-4 flex rounded bg-blue-200 p-2">
               <div className="flex flex-row space-x-2">
                 {/* TODO: Display each accommodation with the component "accomm_segment.tsx" */}
-                {/* <Accomm_Segment/> */}
-
-                {/* TODO: Display image of accommodation */}
-                <img
-                  className="w-1/2 p-2"
-                  src="https://via.placeholder.com/640x640"
-                  alt="placeholder img"
+                <Accomm_Segment
+                  id={accomm.id}
+                  name={accomm.name}
+                  price={accomm.price}
+                  num_of_rooms={accomm.num_of_rooms}
+                  barangay={accomm.barangay}
+                  typeArray={stalsDBstringArray(accomm.typeArray)}
+                  tagArray={stalsDBstringArray(accomm.tagArray)}
                 />
-
-                <div className="w-full bg-blue-200 p-2">
-                  <div className="mb-2 rounded-lg bg-blue-100 p-2">
-                    <p>
-                      {accomm.name} | Archived: {String(accomm.is_archived)}
-                    </p>
-                    <p>Price: {accomm.price}</p>
-                  </div>
-
-                  <div className="flex flex-row space-x-2">
-                    <div className="w-full rounded-lg bg-blue-100">
-                      <p className="p-2 text-center">Category:</p>
-                      <p className="p-2 text-center">
-                        {stalsDBstringArray(accomm.typeArray).join(", ")}
-                      </p>
-                    </div>
-
-                    <div className="w-full rounded-lg bg-blue-100">
-                      <p className="p-2 text-center">Location:</p>
-                      <p className="p-2 text-center">{accomm.location}</p>
-                    </div>
-
-                    <div className="w-full rounded-lg bg-blue-100">
-                      <p className="p-2 text-center">Rooms:</p>
-                      <p className="p-2 text-center">{accomm.num_of_rooms}</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="w-full bg-blue-200 p-2">
-                  <div className="mb-2 rounded-lg bg-blue-100 p-2">
-                    <p>Tags:</p>
-                    <p>{accomm.tags}</p>
-                  </div>
-                  <div className="h-20 w-full bg-blue-200"></div>
-
-                  <div className="flex w-full flex-row space-x-2">
-                    <div className="w-full bg-blue-200"></div>
-
-                    <button
-                      className="rounded border border-gray-400 bg-white p-2"
-                      onClick={() => {
-                        archiveAccomm.mutate({
-                          id: accomm.id,
-                          is_archived: accomm.is_archived,
-                        });
-                      }}
-                    >
-                      Archive
-                    </button>
-
-                    <button
-                      className="rounded border border-gray-400 bg-white p-2"
-                      onClick={() => {
-                        deleteAccomm.mutate({
-                          id: accomm.id,
-                        });
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
               </div>
             </div>
             <br />
