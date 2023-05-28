9a6067f2fc1ab21e96abc3fac95b132d88001de2
vcdayag <100204084+vcdayag@users.noreply.github.com>
2023-05-28 16:18:03 +0800

    feat(front): if user is landlord show management buttons (#419)

--- src/pages/accommodation/[id]/index.tsx
+++ src/pages/accommodation/[id]/index.tsx
@@ -32,10 +32,13 @@ export default function Accommodation() {
   //   status: undefined,
   // });
 
-  const { data: sessionData } = useSession();
+  const { data: userSession } = useSession();
+
   if (accommData === null) {
     return Error404();
   }
+
+  const isLandlordViewing = accommData?.landlord === userSession?.user?.id;
 
   return (
     <div className="scrollbar flex h-screen flex-col overflow-auto bg-p-ngray">
@@ -157,7 +160,6 @@ export default function Accommodation() {
                         stroke="currentColor"
                         className="h-8 w-8"
                       >
-                        {" "}
                         <path
                           stroke-linecap="round"
                           stroke-linejoin="round"
@@ -165,61 +167,63 @@ export default function Accommodation() {
                         />
                       </svg>
                     </label>
-                  </div>
-                  {/* If a landlord is viewing the page */}
-                  <div className="float-right flex hidden gap-1">
-                    {/* Edit button */}
-                    <button type="button" className="accomP-button">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="h-8 w-8 fill-white stroke-p-dblue"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                        />
-                      </svg>
-                    </button>
-                    {/* Archive button */}
-                    <button type="button" className="accomP-button ">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="h-8 w-8 fill-white stroke-p-dblue"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-                        />
-                      </svg>
-                    </button>
+                    {/* If a landlord is viewing the page */}
+                    {isLandlordViewing && (
+                      <div className="float-right flex gap-1">
+                        {/* Edit button */}
+                        <Link className="accomP-button" href={`${id}/edit`}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="h-8 w-8 fill-white stroke-p-dblue"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                            />
+                          </svg>
+                        </Link>
+                        {/* Archive button */}
+                        <button type="button" className="accomP-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="h-8 w-8 fill-white stroke-p-dblue"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                            />
+                          </svg>
+                        </button>
 
-                    {/* Delete button */}
-                    <button type="button" className="accomP-button">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="h-8 w-8 fill-white stroke-p-dblue"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                        />
-                      </svg>
-                    </button>
+                        {/* Delete button */}
+                        <button type="button" className="accomP-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="h-8 w-8 fill-white stroke-p-dblue"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
@@ -345,8 +349,8 @@ export default function Accommodation() {
                         roomIndex={i}
                         status={room.occupied}
                         hidden={
-                          sessionData?.profile.type === "LANDLORD" &&
-                          accommData?.landlord === sessionData?.user?.id &&
+                          userSession?.profile.type === "LANDLORD" &&
+                          accommData?.landlord === userSession?.user?.id &&
                           accommData?.id === room.accommodationId
                             ? false
                             : room.is_archived
@@ -358,8 +362,8 @@ export default function Accommodation() {
                   )}
 
                   {/* TODO: ADD ROOM BUTTON SHOULD ONLY APPEAR IF LANDLORD IS LOOKING AT PAGE */}
-                  {sessionData?.profile.type === "LANDLORD" &&
-                    accommData?.landlord === sessionData?.user?.id && (
+                  {userSession?.profile.type === "LANDLORD" &&
+                    accommData?.landlord === userSession?.user?.id && (
                       <Link
                         href={`/accommodation/${id}/room/add`}
                         className="flex items-stretch"
