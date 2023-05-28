e2d78def105c0dd36e7e2208d623fe27fe2fbf25
jararellano <jrarellano1@up.edu.ph>
2023-05-29 00:11:02 +0800

    feat(front): change username to email for delete account confirmation

--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -61,7 +61,7 @@ const ConfirmationPrompt: React.FC<{
                 type="button"
                 className="mr-2 inline-flex items-center rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800"
               >
-                Confirm
+                Continue
               </button>
               <button
                 onClick={onCancel}
