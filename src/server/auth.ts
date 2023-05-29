d80c7ae05830546da845a4b0640bf5b5c97ce42a
vcdayag <100204084+vcdayag@users.noreply.github.com>
2023-05-29 15:12:07 +0800

    chore: when user signsout ask which account on login (#499)

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -59,6 +59,13 @@ function getProviders() {
       GoogleProvider({
         clientId: env.GOOGLE_CLIENT_ID,
         clientSecret: env.GOOGLE_CLIENT_SECRET,
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+          },
+        },
       }),
     );
   }
