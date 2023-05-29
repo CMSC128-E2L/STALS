654114a3889145fdad0da70c7fa1a5b16640d3ab
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-30 05:31:46 +0800

    feat(front): responsive navbar all pages

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -19,35 +19,35 @@ export const NavBar: React.FC<NavBarProps> = ({ register, name, showBack }) => {
   return (
     <>
       {/* Navigation bar */}
-      <nav className="sticky top-0 z-50 flex w-full flex-col items-center justify-between bg-p-dviolet p-4 py-1 sm:flex-row">
+      <nav className="sticky top-0 z-50 flex w-full flex-wrap items-center justify-between bg-p-dviolet p-4 py-1 sm:flex-row">
         {/* Left side */}
-        <div className="flex space-x-0">
-          {/* div button hack again */}
-          {showBack && (
-            <div
-              className="flex cursor-pointer items-center"
-              onClick={() => {
-                router.back();
-              }}
-            >
-              <div className="relative h-10 w-10">
-                <svg
-                  aria-hidden="true"
-                  fill="none"
-                  stroke="white"
-                  stroke-width="1.5"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-              </div>
+        {showBack && (
+          <div
+            className="flex cursor-pointer items-center"
+            onClick={() => {
+              router.back();
+            }}
+          >
+            <div className="relative h-10 w-10">
+              <svg
+                aria-hidden="true"
+                fill="none"
+                stroke="white"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
             </div>
-          )}
+          </div>
+        )}
+        <div className="flex grow items-center justify-center sm:grow-0">
+          {/* div button hack again */}
           <Link href="/homepage" className="flex items-center">
             <div className="relative h-[3.5rem] w-[3.5rem]">
               <Image
@@ -64,40 +64,38 @@ export const NavBar: React.FC<NavBarProps> = ({ register, name, showBack }) => {
         </div>
 
         {/* Right side */}
-        <div className="w-full md:block md:w-auto">
-          <ul className="mr-2 mt-4 flex items-center p-4 font-medium sm:flex-col md:mt-0 md:flex-row md:space-x-5 md:border-0 md:p-0">
-            <li>
-              {register && name ? (
-                <div className="mr-4 flex items-center sm:mr-0">
-                  <input
-                    {...register(name)}
-                    type="text"
-                    placeholder="Search accommodations..."
-                    className="rounded-l-full px-4 py-2 outline outline-1 outline-white"
-                  />
-                  <button className="rounded-r-full bg-white py-2 pl-2 pr-3 text-white outline outline-1 outline-white">
-                    <svg
-                      className="h-6 w-6 pb-0.5 text-p-dgray hover:text-p-rblue"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <circle cx="11" cy="11" r="8" />
-                      <line x1="21" y1="21" x2="16.65" y2="16.65" />
-                    </svg>
-                  </button>
-                </div>
-              ) : (
-                <div></div>
-              )}
-            </li>
-            <li>
-              <ProfileButton />
-            </li>
-          </ul>
+        <div className="flex grow items-center justify-end p-4 font-medium sm:flex-col md:mt-0 md:flex-row md:space-x-5 md:border-0 md:p-0">
+          <div>
+            {register && name ? (
+              <div className="mr-3 flex items-center sm:mr-0">
+                <input
+                  {...register(name)}
+                  type="text"
+                  placeholder="Search accommodations..."
+                  className="w-56 rounded-l-full px-4 py-2 outline outline-1 outline-white sm:w-fit"
+                />
+                <button className="rounded-r-full bg-white py-2 pl-2 pr-3 text-white outline outline-1 outline-white">
+                  <svg
+                    className="h-6 w-6 pb-0.5 text-p-dgray hover:text-p-rblue"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="11" cy="11" r="8" />
+                    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+                  </svg>
+                </button>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+          <div>
+            <ProfileButton />
+          </div>
         </div>
       </nav>
     </>
