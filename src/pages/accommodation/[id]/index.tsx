8b7eae7245a8e646b9de71a157c531ce2e7921ca
Van Paul Dayag <vcdayag@up.edu.ph>
2023-06-03 21:15:56 +0800

    fix: dont show download to guest

--- src/pages/accommodation/[id]/index.tsx
+++ src/pages/accommodation/[id]/index.tsx
@@ -320,28 +320,30 @@ export default function Accommodation() {
                   </Link>
                 </div>
               )}
-              <label className="cursor-pointer">
-                <button
-                  className="accPButton sr-only mx-3 mb-2 self-end px-3 text-lg"
-                  onClick={() => {
-                    setpdfdownload(true);
-                  }}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-8 w-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+              {!isGuest && (
+                <label className="cursor-pointer">
+                  <button
+                    className="accPButton sr-only mx-3 mb-2 self-end px-3 text-lg"
+                    onClick={() => {
+                      setpdfdownload(true);
+                    }}
                   />
-                </svg>
-              </label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-8 w-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    />
+                  </svg>
+                </label>
+              )}
             </div>
           </div>
         </div>
