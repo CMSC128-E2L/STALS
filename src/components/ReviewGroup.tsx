95d6517b1216fc6fc933a7ea89361502d7ff568c
Van Paul Dayag <vcdayag@up.edu.ph>
2023-06-03 20:01:14 +0800

    fix: make only users submit a review

--- src/components/ReviewGroup.tsx
+++ src/components/ReviewGroup.tsx
@@ -12,6 +12,7 @@ import { useSession } from "next-auth/react";
 import toast from "react-hot-toast";
 import FormError from "./formError";
 import { type z } from "zod";
+import { UserType } from "@prisma/client";
 
 export default function ReviewGroup() {
   const { id } = dynamicRouteID(useRouter());
@@ -62,80 +63,91 @@ export default function ReviewGroup() {
   };
 
   const { data: userSession } = useSession();
+  const isUser = userSession && userSession.profile.type === UserType.USER;
 
-  return (
-    <div className="scrollbar overflow-y-auto pt-8">
-      <div className="grid grid-cols-1 justify-items-stretch gap-4">
-        {userSession !== null && (
-          <div className="w-auto flex-row ">
-            <form
-              // eslint-disable-next-line @typescript-eslint/no-misused-promises
-              onSubmit={handleSubmit(
-                (data: {
-                  accommodationId: string;
-                  rating: number;
-                  review?: string | undefined;
-                  time?: string | undefined;
-                  date?: string | undefined;
-                }) => {
-                  addReview.mutate(data);
-                },
-                (error) => {
-                  // console.log(error);
-                },
-              )}
-            >
-              <div className="mb-4 w-full rounded-[15px] border border-gray-200 bg-gray-50">
-                <div className="grid grid-cols-2 justify-items-stretch">
-                  <div className="flex items-center py-2 pl-4">
-                    <h1 className="text-start text-2xl font-bold">
-                      Write a Review
-                    </h1>
-                  </div>
-                  <div className="flex items-center justify-end pr-4 pt-2 text-gray-800">
-                    {/* <Link
+  const ReviewFrom = () => {
+    return (
+      <>
+        <div className="w-auto flex-row ">
+          <form
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onSubmit={handleSubmit(
+              (data: {
+                accommodationId: string;
+                rating: number;
+                review?: string | undefined;
+                time?: string | undefined;
+                date?: string | undefined;
+              }) => {
+                addReview.mutate(data);
+              },
+              (error) => {
+                // console.log(error);
+              },
+            )}
+          >
+            <div className="mb-4 w-full rounded-[15px] border border-gray-200 bg-gray-50">
+              <div className="grid grid-cols-2 justify-items-stretch">
+                <div className="flex items-center py-2 pl-4">
+                  <h1 className="text-start text-2xl font-bold">
+                    Write a Review
+                  </h1>
+                </div>
+                <div className="flex items-center justify-end pr-4 pt-2 text-gray-800">
+                  {/* <Link
                       href="ye"
                       className="pt-0 text-end text-xxs underline"
                     >
                       Report a problem
                     </Link> */}
-                  </div>
-                </div>
-
-                <div className="rounded-b-lg bg-white px-4 py-2">
-                  <label htmlFor="editor" className="sr-only">
-                    Publish Post
-                  </label>
-                  <textarea
-                    id="editor"
-                    rows={3}
-                    className="block w-full border-0 bg-white px-3 pt-2 text-sm text-gray-800 focus:ring-0"
-                    placeholder="Share your thoughts on the accommodation"
-                    {...register("review")}
-                  ></textarea>
-
-                  <StarRating
-                    totalStars={5}
-                    initialRating={rating}
-                    onChange={handleRatingChange}
-                    refreshComponent={refreshReviewComponent}
-                  />
-                  <FormError error={errors.rating?.message} />
-                </div>
-
-                <div className="grid py-2">
-                  <div className="justify-self-end pr-2">
-                    <button
-                      type="submit"
-                      className="inline-flex items-center justify-self-end rounded-lg bg-p-dviolet px-10 py-2 text-center text-sm font-medium text-white hover:bg-p-dbviolet focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
-                    >
-                      Publish Post
-                    </button>
-                  </div>
                 </div>
               </div>
-            </form>
-          </div>
+
+              <div className="rounded-b-lg bg-white px-4 py-2">
+                <label htmlFor="editor" className="sr-only">
+                  Publish Post
+                </label>
+                <textarea
+                  id="editor"
+                  rows={3}
+                  className="block w-full border-0 bg-white px-3 pt-2 text-sm text-gray-800 focus:ring-0"
+                  placeholder="Share your thoughts on the accommodation"
+                  {...register("review")}
+                ></textarea>
+
+                <StarRating
+                  totalStars={5}
+                  initialRating={rating}
+                  onChange={handleRatingChange}
+                  refreshComponent={refreshReviewComponent}
+                />
+                <FormError error={errors.rating?.message} />
+              </div>
+
+              <div className="grid py-2">
+                <div className="justify-self-end pr-2">
+                  <button
+                    type="submit"
+                    className="inline-flex items-center justify-self-end rounded-lg bg-p-dviolet px-10 py-2 text-center text-sm font-medium text-white hover:bg-p-dbviolet focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
+                  >
+                    Publish Post
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="overflow-y-auto">
+      <div className="grid grid-cols-1 justify-items-stretch gap-4">
+        {isUser ? (
+          <ReviewFrom />
+        ) : (
+          <h1 className="text-2xl font-bold text-p-dbviolet">Reviews</h1>
         )}
         <ReviewList accomId={id} refreshComponent={refreshReviewComponent} />
       </div>
