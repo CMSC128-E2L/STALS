ff0d985b0638390f6e5db47b1dd24761edea2054
vcdayag <100204084+vcdayag@users.noreply.github.com>
2023-06-03 20:28:19 +0800

    fix: make sure tags is not null (#560)

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -8,7 +8,7 @@ export const SearchItem: React.FC<{
   name: string;
   price: string;
   location: string | null;
-  tags: Array<string>;
+  tags: string[];
 }> = ({ id, name, price, location, tags }) => {
   const [imgSrc, setImgSrc] = useState(
     // `https://stals-worker.p0lbang.workers.dev/${id}.jpg`,
@@ -16,22 +16,6 @@ export const SearchItem: React.FC<{
   );
 
   return (
-    // <Link href={`/accommodation/${id}`}>
-    //   <div className="relative -z-10 ml-3 mt-3 h-64 w-64 rounded-xl border bg-p-gray">
-    //     <Image
-    //       src={imgSrc}
-    //       alt={name}
-    //       fill
-    //       className="-z-10 object-contain"
-    //       unoptimized
-    //       onError={() => {
-    //         setImgSrc(placeholder.src);
-    //       }}
-    //     />
-    //     <div className="z-20 text-center">{name}</div>
-    //   </div>
-    // </Link>
-
     <div className="m-4 flex justify-center self-stretch">
       <Link href={`/accommodation/${id}`} className="h-full grow">
         <div className="h-full grow flex-col items-center rounded-xl border-2 border-solid bg-gray-50 shadow-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-p-lviolet sm:w-72">
@@ -53,24 +37,20 @@ export const SearchItem: React.FC<{
               <div className="mb-2 text-xl font-bold">{name}</div>
               <p className="text-xl">Php {price}</p>
               <p className="mb-4 text-xl">{location}</p>
-              {/* {tags !== "" ? (
-                
-              ) : (
-                <span className="bg-gray inline-block"></span>
-              )} */}
             </div>
             <div className="w-full">
               <span className="object-contain">
-                {tags
-                  .filter((tag) => tag !== "")
-                  .map((tag, index, array) => (
-                    <span
-                      key={tag}
-                      className="mb-2 mr-2 inline-block rounded-full bg-p-lviolet px-3 py-1 text-center text-sm font-semibold text-gray-700 shadow shadow-p-vdviolet/30"
-                    >
-                      {tag}
-                    </span>
-                  ))}
+                {tags &&
+                  tags
+                    .filter((tag) => tag !== "")
+                    .map((tag, index) => (
+                      <span
+                        key={index}
+                        className="mb-2 mr-2 inline-block rounded-full bg-p-lviolet px-3 py-1 text-center text-sm font-semibold text-gray-700 shadow shadow-p-vdviolet/30"
+                      >
+                        {tag}
+                      </span>
+                    ))}
               </span>
             </div>
           </div>
