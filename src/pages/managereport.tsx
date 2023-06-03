59f4fee1f59abb4955abe6a4643cfc4fa8327aed
jararellano <100275806+jararellano@users.noreply.github.com>
2023-06-04 01:31:22 +0800

    feat(front):profile clickable reported accomm (#566)

--- src/pages/managereport.tsx
+++ src/pages/managereport.tsx
@@ -6,7 +6,8 @@ import Error401 from "~/pages/401";
 import { useSession } from "next-auth/react";
 import { UserType } from "@prisma/client";
 import toast from "react-hot-toast";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import Link from "next/link";
 
 export default function ManageReport() {
   const userSession = useSession({ required: true });
@@ -69,58 +70,60 @@ export default function ManageReport() {
                 {accomm_reports.map((report) => {
                   return (
                     <div key={report.id} className="mb-6">
-                      <div className="float-right">
-                        <button
-                          type="button"
-                          className=""
-                          onClick={() => {
-                            archiveAccom.mutate({
-                              id: report.reported_id,
-                              is_archived: false,
-                            });
-                            delReport.mutate(report.id);
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="h-8 w-8"
+                      <Link href="">
+                        <div className="float-right">
+                          <button
+                            type="button"
+                            className=""
+                            onClick={() => {
+                              archiveAccom.mutate({
+                                id: report.reported_id,
+                                is_archived: false,
+                              });
+                              delReport.mutate(report.id);
+                            }}
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-8 w-8"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                        <div className="mb-2 flex flex-row">
+                          <div className="relative flex h-[2.5rem] w-[2.5rem] flex-col">
+                            <Image
+                              src={user.src}
+                              className="flex rounded-full object-contain"
+                              alt="User Photo"
+                              fill
                             />
-                          </svg>
-                        </button>
-                      </div>
-                      <div className="mb-2 flex flex-row">
-                        <div className="relative flex h-[2.5rem] w-[2.5rem] flex-col">
-                          <Image
-                            src={user.src}
-                            className="flex rounded-full object-contain"
-                            alt="User Photo"
-                            fill
-                          />
+                          </div>
+                          <div className="ml-4 ">
+                            <p className="mb-1 flex text-sm font-semibold">
+                              {report.user.first_name} {report.user.middle_name}{" "}
+                              {report.user.last_name} {report.user.Suffix}
+                            </p>
+                            <p className="flex text-xs text-gray-400">
+                              {report.type_reported}
+                            </p>
+                          </div>
                         </div>
-                        <div className="ml-4 ">
-                          <p className="mb-1 flex text-sm font-semibold">
-                            {report.user.first_name} {report.user.middle_name}{" "}
-                            {report.user.last_name} {report.user.Suffix}
-                          </p>
-                          <p className="flex text-xs text-gray-400">
-                            {report.type_reported}
+                        <div className="mb-4 ml-14 flex">
+                          <p className="flex text-base">
+                            reported {report.reported_name}
                           </p>
                         </div>
-                      </div>
-                      <div className="mb-4 ml-14 flex">
-                        <p className="flex text-base">
-                          reported {report.reported_name}
-                        </p>
-                      </div>
+                      </Link>
                     </div>
                   );
                 })}
