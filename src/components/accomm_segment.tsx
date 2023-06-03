9ac6f33d0b061fc7dbc4bec6bce6d32baa7390e9
Lawrence Oliver Marquez <70372867+lawmarquez@users.noreply.github.com>
2023-06-03 10:41:11 +0800

    feat(front): add rooms in management (#550)

--- src/components/accomm_segment.tsx
+++ src/components/accomm_segment.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { api } from "~/utils/api";
 import Link from "next/link";
+import RoomButton from "~/components/roomButton";
+
 import placeholder from "public/images/logo d-violet.png";
+import { useSession } from "next-auth/react";
+import ConfirmationPrompt from "./prompt";
 
 {
   /* TODO: Tweak data types to be displayed for each variable in the component */
@@ -36,6 +40,10 @@ const Accomm_Segment: React.FC<{
     // `https://stals-worker.p0lbang.workers.dev/${id}.jpg`,
     `https://stals-worker.p0lbang.workers.dev/api/v2/${id}/${id}`,
   );
+  const { data: userSession } = useSession();
+  const { data: accommData, isLoading: accommLoading } =
+    api.accommodation.getOneRelations.useQuery(id);
+  const [showDelPrompt, setShowDelPrompt] = useState(false);
 
   const archiveAccomm = api.accommodation.archive.useMutation({
     onSuccess: () => {
@@ -52,7 +60,7 @@ const Accomm_Segment: React.FC<{
   });
 
   return (
-    <div className="w-full flex-shrink-0 p-4">
+    <div className="w-[920px] flex-shrink-0 p-4">
       <div className="mx-auto rounded border-2 bg-white p-2 shadow-md">
         <div className="flex flex-row space-x-2">
           <img
@@ -101,13 +109,13 @@ const Accomm_Segment: React.FC<{
                 <p className="p-2 text-center text-white">{location}</p>
               </div>
 
-              <div className="w-[100px] w-full rounded-lg bg-p-dviolet shadow-md">
+              {/* <div className="w-[100px] w-full rounded-lg bg-p-dviolet shadow-md">
                 {/* NUM OF ROOMS */}
-                <p className="mb-0.5 p-2 text-center text-xl font-bold text-white">
+              {/* <p className="mb-0.5 p-2 text-center text-xl font-bold text-white">
                   Rooms:
                 </p>
                 <p className="p-2 text-center text-white">{num_of_rooms}</p>
-              </div>
+              </div> */}
             </div>
           </div>
 
@@ -173,15 +181,87 @@ const Accomm_Segment: React.FC<{
               {/* DELETE */}
               <button
                 className="rounded-md bg-p-dbviolet p-2 text-xl font-bold text-white shadow"
-                onClick={() => {
-                  deleteAccomm.mutate({
-                    id: id,
-                  });
-                }}
+                // onClick={() => {
+                //   deleteAccomm.mutate({
+                //     id: id,
+                //   });
+                // }}
+                onClick={() => setShowDelPrompt(true)}
               >
                 Delete
               </button>
+              {showDelPrompt && (
+                <ConfirmationPrompt
+                  onConfirm={() => {
+                    deleteAccomm.mutate({
+                      id: id,
+                    });
+                  }}
+                  onCancel={() => setShowDelPrompt(false)}
+                  message="Are you sure you want to delete this accommodation? "
+                  submessage="This action cannot be undone."
+                />
+              )}
             </div>
+          </div>
+        </div>
+        <div className="flex flex-shrink-0 justify-center">
+          <div className="scrollbar flex flex-row items-stretch space-x-3 overflow-x-scroll p-3">
+            {accommData?.Room && accommData?.Room.length > 0 ? (
+              accommData?.Room.map((room, i: number) => (
+                <RoomButton
+                  key={room.id}
+                  id={room.id}
+                  roomIndex={i}
+                  status={room.occupied}
+                  hidden={
+                    userSession?.profile.type === "LANDLORD" &&
+                    accommData?.landlord === userSession?.user?.id &&
+                    accommData?.id === room.accommodationId
+                      ? false
+                      : room.is_archived
+                  }
+                  roomAccID={room.accommodationId}
+                  roomAvail={room.occupied}
+                  roomPrice={room.price}
+                  roomBeds={room.num_of_beds}
+                  roomAircon={room.with_aircon}
+                  roomUtils={room.with_utilities}
+                  roomArchive={room.is_archived}
+                />
+              ))
+            ) : (
+              <p className="items-center justify-center">
+                No rooms are available yet.
+              </p>
+            )}
+
+            {/* TODO: ADD ROOM BUTTON SHOULD ONLY APPEAR IF LANDLORD IS LOOKING AT PAGE */}
+            {userSession?.profile.type === "LANDLORD" &&
+              accommData?.landlord === userSession?.user?.id && (
+                <Link
+                  href={`/accommodation/${id}/room/add`}
+                  className="flex items-stretch"
+                >
+                  <button className="flex flex-col items-center justify-center rounded-lg border-2 border-dashed border-p-black/50 px-8">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                    <label className="text-xs">Add Room</label>
+                  </button>
+                </Link>
+              )}
           </div>
         </div>
       </div>
