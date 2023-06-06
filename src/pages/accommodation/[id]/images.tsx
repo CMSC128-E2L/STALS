c469524cae909f5b5bba9357d8d9aeb91fec20ee
Van Paul Dayag <vcdayag@up.edu.ph>
2023-06-06 20:40:35 +0800

    fix: svg errors

--- src/pages/accommodation/[id]/images.tsx
+++ src/pages/accommodation/[id]/images.tsx
@@ -185,11 +185,11 @@ export default function EditAccommodation() {
                             width="20"
                             height="20"
                             viewBox="0 0 24 24"
-                            stroke-width="2"
+                            strokeWidth="2"
                             stroke="currentColor"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           >
                             <path
                               stroke="none"
