b34d3c3e0e6e332b81f4c14fa7949e9e102b751e
Jamie Mari Ciron <jociron@up.edu.ph>
2023-06-06 15:33:01 +0800

    feat (front): added icons for buttons

--- src/components/accomm_segment.tsx
+++ src/components/accomm_segment.tsx
@@ -181,11 +181,48 @@ const Accomm_Segment: React.FC<{
           <div className="flex justify-end space-x-2">
             {/* Images */}
             <Link className="management-buttons" href={`${id}/images`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-photo-down mr-2 mt-0.5"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M15 8h.01"></path>
+                <path d="M12.5 21h-6.5a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6.5"></path>
+                <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l4 4"></path>
+                <path d="M14 14l1 -1c.653 -.629 1.413 -.815 2.13 -.559"></path>
+                <path d="M19 16v6"></path>
+                <path d="M22 19l-3 3l-3 -3"></path>
+              </svg>
               Images
             </Link>
 
             {/* EDIT */}
             <Link className="management-buttons" href={`${id}/edit`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-edit mr-2 mt-0.5"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+                <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+                <path d="M16 5l3 3"></path>
+              </svg>
               Edit
             </Link>
 
@@ -200,6 +237,22 @@ const Accomm_Segment: React.FC<{
                   });
                 }}
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-reload mr-2 mt-0.5"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747"></path>
+                  <path d="M20 4v5h-5"></path>
+                </svg>
                 Restore
               </button>
             ) : (
@@ -212,6 +265,23 @@ const Accomm_Segment: React.FC<{
                   });
                 }}
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-archive mr-2 mt-0.5"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+                  <path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10"></path>
+                  <path d="M10 12l4 0"></path>
+                </svg>
                 Archive
               </button>
             )}
@@ -219,13 +289,26 @@ const Accomm_Segment: React.FC<{
             {/* DELETE */}
             <button
               className="management-buttons"
-              // onClick={() => {
-              //   deleteAccomm.mutate({
-              //     id: id,
-              //   });
-              // }}
               onClick={() => setShowDelPrompt(true)}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-trash-x mr-2 mt-0.5"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 7h16"></path>
+                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                <path d="M10 12l4 4m0 -4l-4 4"></path>
+              </svg>
               Delete
             </button>
             {showDelPrompt && (
