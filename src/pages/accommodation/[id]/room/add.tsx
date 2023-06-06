736688c0d039d9ae6ec824af6e6d0684488ac6f3
Van Paul Dayag <vcdayag@up.edu.ph>
2023-06-06 20:16:03 +0800

    fix: use button for back

--- src/pages/accommodation/[id]/room/add.tsx
+++ src/pages/accommodation/[id]/room/add.tsx
@@ -162,14 +162,14 @@ export default function AddRoom() {
               </div>
 
               <div>
-                <button
-                  className="flex w-full justify-center rounded-full bg-gray-500 px-4 py-2 font-bold text-white opacity-75 shadow shadow-gray-400"
+                <input
+                  className="flex w-full cursor-pointer justify-center rounded-full bg-gray-500 px-4 py-2 font-bold text-white opacity-75 shadow shadow-gray-400"
+                  type="button"
+                  value="Cancel"
                   onClick={() => {
                     router.back();
                   }}
-                >
-                  Cancel
-                </button>
+                />
               </div>
             </div>
           </form>
